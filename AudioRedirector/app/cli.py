a46bbd8fc515c0edd7f3b51e26f606fd430f44c2
0x07cb <83157348+0x07CB@users.noreply.github.com>
2024-06-08 16:50:37 +0200

    update script 'cli.py'

--- AudioRedirector/app/cli.py
+++ AudioRedirector/app/cli.py
@@ -16,36 +16,56 @@ AUDIO_SOURCE_TRANSMIT_STATUS = False
 # define a variable to store the counts of the number of times the amplitude is greater than 2.0
 AUDIO_SOURCE_TRANSMIT_COUNT = 0
 
-def callback_audio_source(indata, frames, time, status):
+# audio_effect_adjust_pitch_and_speed(indata_array, pitch=1.0, speed=1.0):
+
+def get_amplitude_audio_signal(indata_array):
+    return np.abs(indata_array).mean() * 200
+
+def normalize_audio_signal(indata_array):
+    return indata_array / np.max(np.abs(indata_array))
+
+def increase_gain_audio_signal(indata_array, gain=1.0):
+    return indata_array * gain
+
+def decrease_gain_audio_signal(indata_array, gain=1.0):
+    return indata_array / gain
+
+
+
+def get_dB_audio_signal(indata_array):
+    return 20 * np.log10(np.abs(indata_array))
+
+def get_dBFS_audio_signal(indata_array):
+    return 20 * np.log10(np.abs(indata_array) / np.max(np.abs(indata_array)))
+
+def max_limit_dB_audio_signal(indata_array, max_dB=0.0):
+    # Get the dB of the audio signal
+    dB_audio_signal = get_dB_audio_signal(indata_array=indata_array)
+    # if `dB_audio_signal` is greater than `max_dB`, then ajust all values in `indata_array` (if too high, then reduce the values). In resume if the audio signal is superior to the limit, then the audio signal is set to be equal to the limit.
+    return np.where(dB_audio_signal > max_dB, max_dB, indata_array) 
+
+def get_n_last_amplitudes(n):
+    global AUDIO_SOURCE_AMPLITUDES
+    return AUDIO_SOURCE_AMPLITUDES[-n:]
+
+def get_mean_amplitudes(amplitudes):
+    return np.mean(amplitudes)
+
+def keep_last_n_amplitudes(n):
+    global AUDIO_SOURCE_AMPLITUDES
+    return AUDIO_SOURCE_AMPLITUDES[-n:]
+
+
+
+def is_audio_signal_transmit(n_last_amplitudes=100,
+                             max_keep_last_amplitudes=200):
     global AUDIO_SOURCE_AMPLITUDES
     global AUDIO_SOURCE_TRANSMIT_STATUS
     global AUDIO_SOURCE_TRANSMIT_COUNT
 
-
-    if status:
-        print(status)
-
-    # I want display the audio amplitude (0.0 to 200.0)
-    # First: Get indata and create a numpy array.
-    # Second: Get the amplitude of the audio signal.
-    # Third: Display the amplitude.
-
-    # First
-    indata_array = np.array(indata)
-
-    # Second
-    amplitude = np.abs(indata_array).mean() * 200
-    AUDIO_SOURCE_AMPLITUDES.append(amplitude)
-
-    # Third
-    print(f"Amplitude: {amplitude:.2f}")
-
-    #udp_socket.sendto(indata.tobytes(), (udp_target_ip, udp_target_port))
-    
-    
-    if ( len(AUDIO_SOURCE_AMPLITUDES) >= 100 ):
-        # Calcule la moyenne des amplitudes des 10 dernières valeurs dans `AUDIO_SOURCE_AMPLITUDES`
-        mean_last_amplitudes = np.mean(AUDIO_SOURCE_AMPLITUDES[-100:])
+    if ( len(AUDIO_SOURCE_AMPLITUDES) >= n_last_amplitudes ):
+        # Calcule la moyenne des amplitudes des `n_last_amplitudes` dernières valeurs dans `AUDIO_SOURCE_AMPLITUDES`
+        mean_last_amplitudes = get_mean_amplitudes(amplitudes=get_n_last_amplitudes(n=n_last_amplitudes))
         if mean_last_amplitudes >= 2.0:
             AUDIO_SOURCE_TRANSMIT_COUNT += 1
             if ( AUDIO_SOURCE_TRANSMIT_COUNT > 100 ):
@@ -67,17 +87,46 @@ def callback_audio_source(indata, frames, time, status):
                     AUDIO_SOURCE_TRANSMIT_STATUS = False
                     AUDIO_SOURCE_TRANSMIT_COUNT = 0
 
-        if ( len(AUDIO_SOURCE_AMPLITUDES) >= 200 ):
+        if ( len(AUDIO_SOURCE_AMPLITUDES) > max_keep_last_amplitudes ):
             # remove the first 5 values ( keep the last >=95 values. )
-            AUDIO_SOURCE_AMPLITUDES = AUDIO_SOURCE_AMPLITUDES[-195:]
+            AUDIO_SOURCE_AMPLITUDES = keep_last_n_amplitudes(n=max_keep_last_amplitudes)
 
 
+def callback_audio_source(indata, frames, time, status):
+    global AUDIO_SOURCE_AMPLITUDES
+    global AUDIO_SOURCE_TRANSMIT_STATUS
+    global AUDIO_SOURCE_TRANSMIT_COUNT
 
-    if ( AUDIO_SOURCE_TRANSMIT_STATUS == True ):
-        q__audio.put(indata.copy())
-    else:
-        q__audio.put(np.zeros_like(indata))
+    if status:
+        print(status)
 
+    # I want display the audio amplitude (0.0 to 200.0)
+    # First: Get indata and create a numpy array.
+    # Second: Get the amplitude of the audio signal.
+    # Third: Display the amplitude.
+
+    # First
+    indata_array = np.array(indata)
+
+    # Second
+    amplitude = get_amplitude_audio_signal(indata_array=indata_array)
+    #AUDIO_SOURCE_AMPLITUDES.append(amplitude)
+
+    # Third
+    print(f"Amplitude: {amplitude:.2f}")
+
+    #udp_socket.sendto(indata.tobytes(), (udp_target_ip, udp_target_port))
+  
+    indata = increase_gain_audio_signal(indata_array, gain=12.0)
+
+
+    # if ( AUDIO_SOURCE_TRANSMIT_STATUS == True ):
+    #     q__audio.put(indata.copy())
+    # else:
+    #     q__audio.put(np.zeros_like(indata))
+
+
+    q__audio.put(indata.copy())
     q__file.put(indata.copy())
 
 def callback_audio_destination(outdata, frames, time, status):
