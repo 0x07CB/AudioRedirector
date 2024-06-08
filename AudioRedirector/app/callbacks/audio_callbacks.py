e6299f9f03617a1a525a58228bb84282e328e794
0x07cb <83157348+0x07CB@users.noreply.github.com>
2024-06-08 19:28:44 +0200

    Updated Audio Callbacks

--- AudioRedirector/app/callbacks/audio_callbacks.py
+++ AudioRedirector/app/callbacks/audio_callbacks.py
@@ -1,9 +1,13 @@
 #coding: utf-8
 import numpy as np
+import sys
 import queue
 from audio_utils.signal_processing import get_dB_audio_signal, get_dBFS_audio_signal
 from audio_processing.limiting import max_limit_dB_audio_signal
 from statistical_utils.amplitude_stats import get_mean_amplitudes
+from effects.audio_amplitude import get_amplitude_audio_signal, increase_gain_audio_signal, decrease_gain_audio_signal, normalize_audio_signal
+
+
 
 class AudioCallbacks(object):
     def __init__(self):
@@ -13,15 +17,17 @@ class AudioCallbacks(object):
         self.q__audio = queue.Queue()
         self.q__file = queue.Queue()
 
+    
+
     def callback_audio_source(self, indata, frames, time, status):
         if status:
             print(status)
 
         indata_array = np.array(indata)
-        amplitude = self.get_amplitude_audio_signal(indata_array=indata_array)
+        amplitude = get_amplitude_audio_signal(indata_array=indata_array)
         print(f"Amplitude: {amplitude:.2f}")
 
-        indata = self.increase_gain_audio_signal(indata_array, gain=12.0)
+        indata = increase_gain_audio_signal(indata_array, gain=12.0)
         self.q__audio.put(indata.copy())
         self.q__file.put(indata.copy())
 
